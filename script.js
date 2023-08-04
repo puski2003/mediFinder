59ff96689460c8ebde40f9c8ee5629bf36b129d8
puski2003 <pasidurajapaksha202@gmail.com>
2023-08-04 17:34:28 +0530

    2nd part header

---
+++ script.js
@@ -0,0 +1,9 @@
+function search_anime(){
+    anime({
+        targets: '.search-bar-group ',
+        width:['25vw','50vw'],
+        easing: 'easeInOutQuad', // Easing function for smooth animation
+    duration: 800, // 1 second
+       
+      });
+}
